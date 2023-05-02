32045e92b499f0f325d4b5157104406c50dc87de
L-Arscott <lawrence.arscott@neuf.fr>
2023-05-02 18:23:43 +0100

    Now also displays zone type.

--- Poor_locations.py
+++ Poor_locations.py
@@ -1,4 +1,4 @@
-## Locations on map by quality
+## Locations on map by cluster
 import mysql.connector
 import pandas as pd
 import geopandas as gpd
@@ -20,7 +20,7 @@ def map_bathing_quality():
     mycursor = mydb.cursor()
 
     # Create query
-    query = "SELECT nameText, lon, lat, quality2021 " \
+    query = "SELECT nameText, specialisedZoneType, lon, lat, quality2021 " \
             "FROM fr_bw2021 WHERE lon BETWEEN -20 AND 20 AND lat BETWEEN 30 AND 60 AND quality2021 <> 'Not classified'"
 
     # Issue SQL queries by calling the execute() method of the cursor object:
@@ -30,23 +30,23 @@ def map_bathing_quality():
     myresult = mycursor.fetchall()
 
     # Create a pandas DataFrame from the list of tuples:
-    df = pd.DataFrame(myresult, columns=['nameText', 'lon', 'lat', 'quality2021'])
+    df = pd.DataFrame(myresult, columns=['nameText', 'specialisedZoneType', 'lon', 'lat', 'quality2021'])
     print(df.value_counts('quality2021'))  # Provide a count
+    print(df.value_counts('specialisedZoneType'))
 
     # Plotting data
     # load map of France using geopandas
     france = gpd.read_file('Data/Maps/gadm41_FRA_1.shp')
-    print(france.head())
 
     # create a geopandas dataframe for locations
-    locations = gpd.GeoDataFrame(geometry=gpd.points_from_xy(df['lon'], df['lat']),
-                                 data=df[['nameText', 'quality2021']])
-    print(locations.head())
+    gdf = gpd.GeoDataFrame(geometry=gpd.points_from_xy(df['lon'], df['lat']),
+                           data=df[['nameText', 'specialisedZoneType', 'quality2021']])
+    print(gdf.head())
 
-    # group by quality, in our desired order
+    # group by cluster, in our desired order
     desired_order = {'Excellent': 0, 'Good': 1, 'Sufficient': 2, 'Poor': 3}
     qualities = pd.Categorical(df['quality2021'], categories=desired_order, ordered=True)
-    locations = locations.groupby(qualities)
+    locations = gdf.groupby(qualities)
 
     # plot the map and locations
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -57,8 +57,31 @@ def map_bathing_quality():
     for quality, dff in locations:
         dff.plot(ax=ax, markersize=2, color=colors[quality], label=f'{quality} quality')
 
+    plt.title('Bathing water quality')
     plt.legend()
     plt.show()
+
+    # we now display bathing water types
+    locations = gdf.groupby('specialisedZoneType')
+
+    # plot the map and locations
+    fig, ax = plt.subplots(figsize=(10, 10))
+    france.plot(ax=ax, alpha=0.4, color='grey')
+
+    # we plot different types in different colours
+    colors = {'coastalBathingWater': 'blue', 'riverBathingWater': 'green', 'lakeBathingWater': 'purple',
+              'transitionalBathingWater': 'red'}
+    for water_type, dff in locations:
+        dff.plot(ax=ax, markersize=2, color=colors[water_type], label=f'{water_type[:-12]}')
+
+    plt.title('Bathing water type')
+    plt.legend()
+    plt.show()
+
+    # conduct some simple statistics
+    for water_type, dff in locations:
+        print(water_type)
+        print(dff['quality2021'].value_counts())
 
 
 map_bathing_quality()
